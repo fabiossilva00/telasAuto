5cba8d360a23148a8711b864757f5bbeffd78e15
fabiossilva00 <fabiossilva00@gmail.com>
2018-03-05 16:22:58 -0300

    Tela inicial Ok

--- telasAuto/ViewController.swift
+++ telasAuto/ViewController.swift
@@ -9,9 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stackLogin: UIStackView!
+    @IBOutlet weak var stackButtons: UIStackView!
+    
+    let screen = UIScreen.main.bounds
+    
+    //0 - Stack Login
+    //1 - Stack Buttons
+    
+    func tamanhoHeight() -> Array<Int> {
+        let screenHeight = screen.height
+        
+        switch screenHeight {
+        case 568:
+            return [13, 26]
+        case 667:
+            return [19, 33]
+        case 812:
+            return [21, 53]
+        case 736:
+            return [27, 37]
+        default:
+            return [13, 10]
+        }
+        
+    }
+    
+    func atualizaTela() {
+        
+        let tamanho = tamanhoHeight()
+        
+        stackLogin.spacing = CGFloat(CFloat(tamanho[0]))
+        stackButtons.spacing = CGFloat(CFloat(tamanho[1]))
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        atualizaTela()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
